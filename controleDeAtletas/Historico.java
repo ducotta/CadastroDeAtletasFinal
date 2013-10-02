fbfd4e0649fdfdd0c726334f3a06874192d596df
Eduardo L Cotta <ducotta@gmail.com>
2013-10-01 22:39:07 -0300

    Concluindo classes Historico e PilotoAutomotivo

--- controleDeAtletas/Historico.java
+++ controleDeAtletas/Historico.java
@@ -1,5 +1,32 @@
 package controleDeAtletas;
 
 public class Historico {
+	
+	private String categoria;
+	private String periodoDeParticipacao;
+	
+	public Historico(String categoria, String periodoDeParticipacao){
+		this.categoria = categoria;
+		this.periodoDeParticipacao = periodoDeParticipacao;
+	}
+	
+	public String getCategoria(){
+		return categoria;
+	}
+	public void setCategoria(String categoria){
+		this.categoria = categoria;
+	}
+	
+	public String getPeriodoDeParticipacao(){
+		return periodoDeParticipacao;
+	}
+	public void setPeriodoDeParticipacao(String periodoDeParticipacao){
+		this.periodoDeParticipacao = periodoDeParticipacao;
+	}
+	
+	@Override
+	public String toString(){
+		return "Correu na categoria " + this.categoria + " " + this.periodoDeParticipacao;
+	}
 
 }
--- controleDeAtletas/PilotoAutomotivo.java
+++ controleDeAtletas/PilotoAutomotivo.java
@@ -1,5 +1,112 @@
 package controleDeAtletas;
 
-public class PilotoAutomotivo {
+import java.util.ArrayList;
+
+public class PilotoAutomotivo extends Atleta {
+	
+	private char classe; // J=Junior N=Novato G=Graduado P=Proficional
+	private String categoria;
+	private String licenca;
+	private int anosDeExperiencia;
+	private ArrayList<Historico> historico;
+	private ArrayList<Premiacao> preiacoes;
+	private int totalVitorias;
+	private int totalPolePositions;
+	private int totalVoltaDeMelhorTempo;
+	private int totalDeBatidas;
+	private int batakScore;
+	
+	public PilotoAutomotivo (String nome){
+		super(nome);
+	}
+	
+	public char getClasse(){
+		return classe;
+	}
+	
+	public void setClasse(char classe){
+		this.classe = classe;
+	}
+	
+	public String getCategoria() {
+		return categoria;
+	}
+	
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	public String getLicenca() {
+		return licenca;
+	}
+	
+	public void setLicenca(String licenca) {
+		this.licenca = licenca;
+	}
+	
+	public int getAnosDeExperiencia() {
+		return anosDeExperiencia;
+	}
+	
+	public void setAnosDeExperiencia(int anosDeExperiencia) {
+		this.anosDeExperiencia = anosDeExperiencia;
+	}
+	
+	public ArrayList<Historico> getHistorico() {
+		return historico;
+	}
+	
+	public void setHistorico(ArrayList<Historico> historico) {
+		this.historico = historico;
+	}
+	
+	public ArrayList<Premiacao> getPreiacoes() {
+		return preiacoes;
+	}
+	
+	public void setPreiacoes(ArrayList<Premiacao> preiacoes) {
+		this.preiacoes = preiacoes;
+	}
+	
+	public int getTotalVitorias() {
+		return totalVitorias;
+	}
+	
+	public void setTotalVitorias(int totalVitorias) {
+		this.totalVitorias = totalVitorias;
+	}
+	
+	public int getTotalPolePositions() {
+		return totalPolePositions;
+	}
+	
+	public void setTotalPolePositions(int totalPolePositions) {
+		this.totalPolePositions = totalPolePositions;
+	}
+	
+	public int getTotalMelhorTempo() {
+		return totalVoltaDeMelhorTempo;
+	}
+	
+	public void setTotalMelhorTempo(int totalMelhorTempo) {
+		this.totalVoltaDeMelhorTempo = totalMelhorTempo;
+	}
+	
+	public int getTotalDeBatidas(){
+		return totalDeBatidas;
+	}
+	
+	public void setTotalDeBatidas(int totalDeBatidas){
+		this.totalDeBatidas = totalDeBatidas;
+	}
+	
+	public int getBatakScore() {
+		return batakScore;
+	}
+	
+	public void setBatakScore(int batakScore) {
+		this.batakScore = batakScore;
+	}
+	
 
 }
