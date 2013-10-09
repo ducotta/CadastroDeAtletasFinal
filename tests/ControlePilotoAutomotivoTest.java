ec879404d23c34ba3ef3b95f6a7e40035776a7ab
Eduardo L Cotta <ducotta@gmail.com>
2013-10-09 14:06:04 -0300

    Testando as funções pesquisar, adicionar e remover da lista de pilotos

--- tests/ControlePilotoAutomotivoTest.java
+++ tests/ControlePilotoAutomotivoTest.java
@@ -13,19 +13,34 @@ import controleDeAtletas.PilotoAutomotivo;
 public class ControlePilotoAutomotivoTest {
 	
 	ControlePilotoAutomotivo umControle;
+	PilotoAutomotivo umPiloto;
 	private ArrayList<PilotoAutomotivo> listaPilotos;
 	
 
 	@Before
 	public void setUp() throws Exception {
+		umPiloto = new PilotoAutomotivo("James Hunt");
+		umControle = new ControlePilotoAutomotivo();
 	}
 
 	@Test
-	public void test() {
-		
-		PilotoAutomotivo umPiloto = new PilotoAutomotivo("James Hunt");
-		assertEquals("James Hunt", listaPilotos.add(umPiloto));
+	public void testGetListaPiloto() {
+		assertNotNull(umControle.getListaPilotos());
 	}
 	
-
+	@Test
+	public void testPesquisaAdicionarPiloto() {
+		umControle.adicionar(umPiloto);
+		
+		assertEquals(umPiloto, umControle.pesquisar("James Hunt"));
+		assertNull(umControle.pesquisar("Niki Lauda"));
+	}
+	
+	@Test
+	public void testPesquisaRemoverPiloto(){
+		umControle.adicionar(umPiloto);
+		umControle.remover(umPiloto);
+		
+		assertNull(umControle.pesquisar("James Hunt"));
+	}
 }
